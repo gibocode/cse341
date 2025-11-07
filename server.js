671f07d295cfe0620d8730994fdbf9d5cf4a786e
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-11-08 03:13:22 +0800

    Add contacts routes and database connection.

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const contacts = require('./routes/contacts');
 const app = express();
 
 app.use('/', require('./routes'));
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+// Initialize database and start server
+contacts.initialize((err) => {
+    if (err) {
+        console.error(err);
+    }
+    else {
+        const port = process.env.APP_PORT || 3000;
+        app.listen(port, () => {
+            console.log(`Database connected and server is running on port ${port}`);
+        });
+    }
 });
