2ee8013e6f4dbf48e5594f09309e6bc20887759c
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-11-08 01:59:44 +0800

    Add router.

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-});
+app.use('/', require('./routes'));
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
