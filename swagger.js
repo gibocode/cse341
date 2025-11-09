9611e47a8af60516b9f552385f88466f53a8afbb
Gilbor Camporazo Jr. <gilborcamporazojr@programmer.net>
2025-11-09 22:51:17 +0800

    Update swagger API documentation to use https as a default scheme.

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
         description: 'An API to manage contacts.',
     },
     host: 'localhost:3000',
-    schemes: ['http', 'https'],
+    schemes: ['https', 'http'],
 };
 
 const outputFile = './swagger.json';
